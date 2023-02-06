b39d66063e5b6a4ab87b9902c8d327cfca7defd5
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2023-02-06 16:42:09 +0200

    Update Container.tsx
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -3,4 +3,5 @@ import styles from './Container.module.scss';
 export type ContainerProps = {
   children: React.ReactNode;
 };
+
 export const Container: React.FC<ContainerProps> = ({ children }) => <div className={styles.Container}>{children}</div>;
