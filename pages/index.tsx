f71f4788fd16a1bd5efeaceb1ea562ec912393a2
IVS737 <ivan.salata@zapal.tech>
2023-02-06 10:19:35 +0200

    feat: add hero section to home page
    ZPL-8 ZPL-9

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,14 @@ import { Namespace } from 'i18n';
 
 export async function getStaticProps({ locale }) {
   return {
-    props: { ...(await serverSideTranslations(locale, [Namespace.Common, Namespace.Languages, Namespace.Navigation])) },
+    props: {
+      ...(await serverSideTranslations(locale, [
+        Namespace.Common,
+        Namespace.Languages,
+        Namespace.Navigation,
+        Namespace.Home,
+      ])),
+    },
   };
 }
 
---
+++ views/HomePage/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,143 @@
+import { useTranslation } from 'next-i18next';
+import { Fragment, useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import { Engine } from 'tsparticles-engine';
+
+import { Text } from 'components';
+
+import { Namespace } from 'i18n';
+
+import styles from './HeroSection.module.scss';
+
+const particlesConfig = {
+  particles: {
+    number: { value: 60, density: { enable: true, value_area: 800 } },
+    color: { value: '#FFF' },
+    shape: { type: 'circle' },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 120,
+      color: '#FFF',
+      opacity: 1,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: { enable: true, mode: 'grab' },
+      onclick: { enable: true, mode: 'bubble' },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 200, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 3, duration: 1, opacity: 0, speed: 3 },
+      repulse: { distance: 300, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+};
+
+export const HeroSection: React.FC = () => {
+  const { t } = useTranslation(Namespace.Home);
+
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <div className={styles.Hero}>
+      <Particles
+        id="particles"
+        className={styles.Hero__Particles}
+        options={{
+          fullScreen: false,
+          particles: {
+            number: { value: 60, density: { enable: true, value_area: 800 } },
+            color: { value: '#FFF' },
+            shape: { type: 'circle' },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
+            },
+            line_linked: {
+              enable: true,
+              distance: 120,
+              color: '#FFF',
+              opacity: 1,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: 'none',
+              random: true,
+              straight: false,
+              out_mode: 'out',
+              bounce: false,
+              attract: { enable: false, rotateX: 600, rotateY: 600 },
+            },
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: { enable: true, mode: 'grab' },
+              onclick: { enable: true, mode: 'bubble' },
+              resize: true,
+            },
+            modes: {
+              grab: { distance: 200, line_linked: { opacity: 1 } },
+              bubble: { distance: 400, size: 3, duration: 1, opacity: 0, speed: 3 },
+              repulse: { distance: 300, duration: 0.4 },
+              push: { particles_nb: 4 },
+              remove: { particles_nb: 2 },
+            },
+          },
+          retina_detect: true,
+        }}
+        init={particlesInit}
+      />
+
+      <div className={styles.Hero__Box}>
+        <Text type="h1" size="heading1" className={styles.Hero__Article}>
+          {t('hero.upperTitle')}
+          <br />
+          {t('hero.lowerTitle')}
+        </Text>
+
+        <Text size="small" className={styles.Hero__SubArticle}>
+          {t('hero.subtitle')}
+        </Text>
+      </div>
+    </div>
+  );
+};
