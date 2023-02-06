153fe3d866a067ef5d4654d4235b7ef497dbbc12
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2023-02-06 10:33:26 +0200

    Update HeroSection.tsx
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>

--- views/HomePage/components/HeroSection/HeroSection.tsx
+++ views/HomePage/components/HeroSection/HeroSection.tsx
@@ -10,57 +10,6 @@ import { Namespace } from 'i18n';
 
 import styles from './HeroSection.module.scss';
 
-const particlesConfig = {
-  particles: {
-    number: { value: 60, density: { enable: true, value_area: 800 } },
-    color: { value: '#FFF' },
-    shape: { type: 'circle' },
-    opacity: {
-      value: 1,
-      random: true,
-      anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
-    },
-    line_linked: {
-      enable: true,
-      distance: 120,
-      color: '#FFF',
-      opacity: 1,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 1,
-      direction: 'none',
-      random: true,
-      straight: false,
-      out_mode: 'out',
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 600 },
-    },
-  },
-  interactivity: {
-    detect_on: 'canvas',
-    events: {
-      onhover: { enable: true, mode: 'grab' },
-      onclick: { enable: true, mode: 'bubble' },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 200, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 3, duration: 1, opacity: 0, speed: 3 },
-      repulse: { distance: 300, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
-  },
-  retina_detect: true,
-};
-
 export const HeroSection: React.FC = () => {
   const { t } = useTranslation(Namespace.Home);
 
