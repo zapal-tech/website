35f6a080c962de80daa71b06110de63124faf74a
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-08 14:36:54 +0200

    refactor: minor style and SEO changes

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -3,7 +3,7 @@ import { Text } from 'components';
 import styles from './Banner.module.scss';
 
 export const Banner: React.FC = () => (
-  <a href="https://u24.gov.ua/" rel="noreferrer noopener" target="_blank" className={styles.Banner}>
+  <a href="https://u24.gov.ua/" rel="noreferrer" target="_blank" className={styles.Banner}>
     <Text className={styles.Banner__Text}>SUPPORT UKRAINE ! STOP WAR</Text>
   </a>
 );
