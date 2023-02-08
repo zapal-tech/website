6a4773b5682f5a0f75d3e51e454bbced797c7c49
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-08 14:37:44 +0200

    feat: input component (wip)

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,38 +1,54 @@
-import { Control, useController } from 'react-hook-form';
+import clsx, { ClassValue } from 'clsx';
+import { Control, useController, UseControllerProps } from 'react-hook-form';
 
 import styles from './Input.module.scss';
 
-export type InputProps = {
-  name: string;
-  id: undefined;
-  label: undefined;
-  control: Control;
-  autoComplete: string;
+export type InputProps = Omit<UseControllerProps, 'control'> & {
+  id?: string;
+  placeholder?: string;
+  className?: ClassValue | ClassValue[];
+  autoComplete?: string;
+  type?: 'text' | 'email' | 'number' | 'search' | 'password' | 'url';
+  control: Control<any, any>;
+  required?: boolean;
+  disabled?: boolean;
 };
 
-export type LabelInputProps = Omit<InputProps, 'id' | 'label'> & {
-  id: string;
-  label: string;
-};
-
-const TextField: React.FC<InputProps | LabelInputProps> = ({ name, id, label, control, autoComplete = 'off' }) => {
-  const { field, fieldState } = useController({ control, name });
+export const Input: React.FC<InputProps> = ({
+  id,
+  placeholder,
+  className,
+  autoComplete = 'off',
+  required = false,
+  disabled = false,
+  ...controllerProps
+}) => {
+  const { field, fieldState } = useController(controllerProps);
 
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => field.onChange(evt.target.value);
 
   return (
-    <div className={styles.Input}>
-      {id && label && (
-        <label className={styles.Input__Label} htmlFor={id}>
-          {label}
+    <div className={clsx(styles.Input, field.value && styles['Input--HasValue'], className)}>
+      <input
+        {...field}
+        className={styles.Input__Field}
+        aria-placeholder={placeholder}
+        id={controllerProps.name || id}
+        autoComplete={autoComplete}
+        onChange={handleChange}
+        required={required}
+        disabled={disabled}
+        placeholder={'\u00A0'}
+      />
+
+      {placeholder && (
+        <label className={styles.Input__Placeholder} htmlFor={controllerProps.name || id}>
+          {placeholder}
+          {required && <span>*</span>}
         </label>
       )}
-
-      <input {...field} id={id} autoComplete={autoComplete} onChange={handleChange} />
 
       {fieldState.error && <span className={styles.Input__Error}>{fieldState.error?.message}</span>}
     </div>
   );
 };
-
-export default TextField;
