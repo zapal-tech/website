6a5d393ff3df190b4822968f5edc70e9b204ed20
IVS737 <ivan.salata@zapal.tech>
2023-02-10 12:28:25 +0200

    feat: add mobile menu

---
+++ components/Header/components/MobileButton/MobileMenu.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import CloseButtonIcon from 'public/icons/close-btn.svg';
+
+import { useAppDispatch, useAppSelector } from 'store';
+import { selectIsMobileMenuOpen, setIsMobileMenuOpen } from 'store/generalSlice';
+
+import { Navigation, SocialLinks } from 'components';
+
+import { HeaderButton } from '../HeaderButton/HeaderButton';
+
+import styles from './MobileMenu.module.scss';
+
+export const MobileMenu: React.FC = () => {
+  const isOpen = useAppSelector(selectIsMobileMenuOpen);
+  const dispatch = useAppDispatch();
+
+  const handleCloseButtonClick = () => dispatch(setIsMobileMenuOpen(false));
+
+  return (
+    <div className={clsx(styles.MobileMenu, isOpen && styles['MobileMenu--Open'])}>
+      <div className={styles.MobileMenu__CloseButton}>
+        <HeaderButton onClick={handleCloseButtonClick}>
+          <CloseButtonIcon />
+        </HeaderButton>
+      </div>
+
+      <Navigation />
+      <button className={styles.MobileMenu__Button}>BECOME A CLIENT</button>
+      <div className={styles.MobileMenu__SocialLinks}>
+        <SocialLinks />
+      </div>
+    </div>
+  );
+};
