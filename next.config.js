2b6173a895907e0a0dab4a457f6915e47a100498
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-10 13:11:50 +0200

    feat: deployment config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/** @type {import('next').NextConfig} */
 
 const pwa = require('next-pwa');
 
@@ -12,10 +11,10 @@ const withPWA = pwa({
 
 const { i18n } = require('./next-i18next.config');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   i18n,
