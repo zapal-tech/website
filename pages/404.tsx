8e2b5ec0c6adad5abd92ae21eca1d5264a654eba
IVS737 <ivan.salata@zapal.tech>
2023-02-12 00:22:21 +0200

    fix: error pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,5 @@
 import { Error404Page } from 'views/Error404Page/Error404Page';
 
 export default function Error404() {
-  return (
-    <>
-      <Error404Page />
-    </>
-  );
+  return <Error404Page />;
 }
