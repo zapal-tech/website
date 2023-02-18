d712fbed8a72aee01e9cf0f871718f9a7a0af9dc
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-19 00:14:14 +0200

    fix(footer): remove unused imports

--- components/Footer/components/Socials/Socials.tsx
+++ components/Footer/components/Socials/Socials.tsx
@@ -1,24 +1,3 @@
-import clsx, { ClassValue } from 'clsx';
-import facebook from 'public/footer/facebook.svg';
-import instagram from 'public/footer/instagram.svg';
-import linkedin from 'public/footer/linkedin.svg';
-import telegram from 'public/footer/telegram.svg';
+import { SocialLinks } from 'components';
 
-import { SocialLinks } from 'components/SocialLinks/SocialLinks';
-
-import styles from './Socials.module.scss';
-
-const IconsArray = [instagram, facebook, linkedin, telegram];
-
-export type SocialsProps = {
-  className?: ClassValue | ClassValue[];
-};
-
-export const Socials: React.FC<SocialsProps> = ({ className }) => (
-  // <div className={clsx(styles.Icons, className)}>
-  //   {IconsArray.map((Icon, index) => (
-  //     <Icon key={index} className={styles.Icons__Icon} />
-  //   ))}
-  // </div>
-  <SocialLinks />
-);
+export const Socials: React.FC = () => <SocialLinks />;
