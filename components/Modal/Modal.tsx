cc18dbc1e2f448c78a1a1b49fec345f65e10f911
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-20 00:40:19 +0200

    feat: improve Modal transition

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,4 +1,6 @@
-import { useCallback, useEffect } from 'react';
+import clsx from 'clsx';
+import CloseIcon from 'public/icons/close.svg';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import { useAppDispatch, useAppSelector } from 'store';
 import { closeModal, selectIsModalOpen, selectModalContent } from 'store/modalSlice';
@@ -12,7 +14,30 @@ export const Modal: React.FC = () => {
   const isOpen = useAppSelector(selectIsModalOpen);
   const content = useAppSelector(selectModalContent);
 
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const [isShow, setIsShow] = useState(false);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const memoizedContent = useMemo(() => content, [isShow]);
+
   const handleCloseModal = useCallback(() => isOpen && dispatch(closeModal()), [dispatch, isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsShow(true);
+    } else {
+      const timeout = setTimeout(() => setIsShow(false), 300);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    // FIXME: Workaround for animation, need to be fixed
+    if (isOpen && isShow) setTimeout(() => modalRef.current?.classList.add(styles['Modal--Open']), 10);
+    if (!isOpen && isShow) modalRef.current?.classList.remove(styles['Modal--Open']);
+  }, [isShow, isOpen]);
 
   useEffect(() => {
     const handleEscKeyDown = (evt: KeyboardEvent) => {
@@ -26,15 +51,13 @@ export const Modal: React.FC = () => {
 
   return (
     <Portal selector="#__next">
-      {isOpen && content && (
-        <div className={styles.Modal}>
-          <div className={styles.Modal__Content}>
-            <button className={styles.Modal__CloseButton} onClick={handleCloseModal}>
-              {/* <CloseIcon /> */}
-            </button>
+      {isShow && (
+        <div className={styles.Modal} ref={modalRef}>
+          <button className={styles.Modal__CloseButton} onClick={handleCloseModal}>
+            <CloseIcon />
+          </button>
 
-            {content}
-          </div>
+          <div className={styles.Modal__Content}>{memoizedContent}</div>
         </div>
       )}
     </Portal>
