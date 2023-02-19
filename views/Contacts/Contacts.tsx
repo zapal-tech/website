af11d76805c9bc1d4e253165019907cd62ebcd86
IVS737 <salata.vanya191@gmail.com>
2023-02-19 19:55:23 +0200

    feat: add new import

--- views/Contacts/Contacts.tsx
+++ views/Contacts/Contacts.tsx
@@ -1,11 +1,9 @@
 import { AppLayout } from 'layouts';
 
 import { Hero } from './components/Hero/Hero';
-import { Locations } from './components/Locations/Locations';
 
 export const Contacts: React.FC = () => (
   <AppLayout>
     <Hero />
-    <Locations />
   </AppLayout>
 );
