ebf5e16c0d82415407b08b659a95da95c6a8f537
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-19 13:50:41 +0200

    feat: translations for error pages

--- views/Error500Page/Error500Page.tsx
+++ views/Error500Page/Error500Page.tsx
@@ -1,13 +1,21 @@
+import { useTranslation } from 'next-i18next';
+
 import { Text } from 'components';
+
+import { Namespace } from 'i18n';
 
 import styles from './Error500Page.module.scss';
 
-export const Error500Page = () => (
-  <div className={styles.Error500Page}>
-    <Text type="h1" size="heading1" className={styles.Error500Page__Text}>
-      500
-    </Text>
+export const Error500Page = () => {
+  const { t } = useTranslation(Namespace.Common);
 
-    <Text size="heading3">Internal Server Error</Text>
-  </div>
-);
+  return (
+    <div className={styles.Error500Page}>
+      <Text type="h1" size="heading1" className={styles.Error500Page__Text}>
+        500
+      </Text>
+
+      <Text size="heading3">{t('internalServerError')}</Text>
+    </div>
+  );
+};
