90c884db5d1193d522ea9538295fb84a3f9fa12a
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-20 18:23:36 +0200

    fix: include locations section to contacts page

--- views/Contacts/Contacts.tsx
+++ views/Contacts/Contacts.tsx
@@ -1,9 +1,11 @@
 import { AppLayout } from 'layouts';
 
 import { Hero } from './components/Hero/Hero';
+import { Locations } from './components/Locations/Locations';
 
 export const Contacts: React.FC = () => (
   <AppLayout>
     <Hero />
+    <Locations />
   </AppLayout>
 );
