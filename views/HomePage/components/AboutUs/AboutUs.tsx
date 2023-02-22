b31bc9e70bf1d1d0b9d369f040e5a2104dd83b56
--global <bohdan.kucheriavyi@zapal.tech>
2023-02-22 17:11:50 +0200

    fix: adaptive about us section

--- views/HomePage/components/AboutUs/AboutUs.tsx
+++ views/HomePage/components/AboutUs/AboutUs.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
 import { Button, Card, Container, Text } from 'components';
 
@@ -8,30 +9,36 @@ import styles from './AboutUs.module.scss';
 
 export const AboutUs: React.FC = () => {
   const { t } = useTranslation(Namespace.Home);
+  const router = useRouter();
+
+  const goToAboutUs = () => router.push('/about');
 
   return (
-    <Container>
+    <Container className={styles.AboutUs}>
       <Text size="heading1" type="h1" uppercase className={styles.AboutUs__Title}>
         {t('about.title')}
       </Text>
+
       <div className={styles.AboutUs__Box}>
         <Text size="heading3" type="h3" className={styles.AboutUs__Subtitle}>
           {t('about.subtitle')}
         </Text>
 
-        <div className={styles.AboutUs__Line}></div>
+        <div className={styles.AboutUs__Line} />
 
-        <div className={styles.AboutUs__CardBox}>
+        <div className={styles.AboutUs__CardsContainer}>
           {(t('about.cards', { returnObjects: true }) as string[]).map((cardText) => (
             <Card key={cardText} frameType="corner">
-              <Text type="h4" className={styles.Card__Text}>
+              <Text type="h4" className={styles.AboutUs__CardText}>
                 {cardText}
               </Text>
             </Card>
           ))}
         </div>
 
-        <Button variant="primary">SERVICES</Button>
+        <Button variant="primary" onClick={goToAboutUs}>
+          {t('about.button')}
+        </Button>
       </div>
     </Container>
   );
