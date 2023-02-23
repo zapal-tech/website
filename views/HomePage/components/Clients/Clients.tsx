3a74b8a6564452ecd9327d0b3bf1eec205157755
Ivan Salata <salata.vanya191@gmail.com>
2023-02-23 14:13:18 +0200

    ZPL-18 Home page Clients and Partners sections (#37)
    * feat: add clients section in json
    
    * feat: add clients section
    
    * feat: add clients section in homepage
    
    * Update home.json
    
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
    
    * Update Clients.module.scss
    
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
    
    * Update Clients.tsx
    
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
    
    ---------
    
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
    Co-authored-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>

---
+++ views/HomePage/components/Clients/Clients.tsx
@@ -0,0 +1,55 @@
+import { useTranslation } from 'next-i18next';
+import { useMemo } from 'react';
+
+import { useWindowSize } from 'hooks';
+
+import { Button, Container, Text } from 'components';
+
+import { Namespace } from 'i18n';
+
+import media from 'styles/media.module.scss';
+
+import styles from './Clients.module.scss';
+
+export const Clients = () => {
+  const windowSize = useWindowSize();
+  const { t } = useTranslation([Namespace.Home, Namespace.Navigation]);
+
+  const isLaptop = useMemo(
+    () => windowSize.width && windowSize.width >= parseInt(media.breakpointLaptop),
+    [windowSize],
+  );
+
+  return (
+    <Container>
+      <div className={styles.Clients}>
+        <Text className={styles.Clients__Title} type="h1" size="heading1" uppercase>
+          {t('clients.title')}
+        </Text>
+
+        {!!isLaptop && (
+          <Text className={styles.Clients__AfterTitleLaptopContent} size="default">
+            {t('clients.titleAfter.upper')}<br />
+            {t('clients.titleAfter.lower')}
+          </Text>
+        )}
+      </div>
+
+      <Text className={styles.Clients__Subtitle} type="h3" size="heading3">
+        {t('clients.subtitle')}
+      </Text>
+
+      <div className={styles.Clients__Slider}>
+        <Text className={styles.Clients__SliderText} type="h3" size="heading2">
+          Slider
+        </Text>
+      </div>
+
+      <Text className={styles.Clients__Text} type="h3" size="heading3">
+        {t('clients.text')}
+      </Text>
+
+      <Button className={styles.Clients__Button}>{t('becomeAClient.title', { ns: Namespace.Navigation })}</Button>
+    </Container>
+  );
+};
