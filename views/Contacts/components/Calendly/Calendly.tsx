424d18686619d77a97799d2721da79769f2226c4
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2023-02-24 14:23:57 +0200

    Update Calendly.tsx
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>

--- views/Contacts/components/Calendly/Calendly.tsx
+++ views/Contacts/components/Calendly/Calendly.tsx
@@ -3,23 +3,14 @@ import { InlineWidget } from 'react-calendly';
 
 import { calendlyInlineConfig } from 'configs/calendly';
 
-// import { useMediaQuery } from 'hooks';
-// import { Card } from 'components';
-// import media from 'styles/media.module.scss';
 import styles from './Calendly.module.scss';
 
 export const Calendly = () => {
   const [mount, setMount] = useState(false);
-  // const isLaptop = useMediaQuery({ width: { min: parseInt(media.laptop) } });
 
   useEffect(() => {
     setMount(true);
   }, []);
-
-  // const calendlyStyles: React.CSSProperties = {
-  // height: 1000,
-  // overflow: 'hidden',
-  // };
 
   return mount ? (
     <div className={styles.Calendly}>
