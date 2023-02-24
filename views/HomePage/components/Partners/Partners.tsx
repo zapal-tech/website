3ab78b5360ed3cdb43c0b144b3f79dfddffd66db
Ivan Salata <salata.vanya191@gmail.com>
2023-02-24 14:39:47 +0200

    ZPL-18 home clients and partners section names fix (#41)
    * feat: add clients section in json
    
    * feat: add clients section
    
    * feat: add clients section in homepage
    
    * feat: add new section in ua json
    
    * fix: namings
    
    * fix: invalid merge
    
    * Update home.json
    
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
    
    ---------
    
    Signed-off-by: Ivan Salata <salata.vanya191@gmail.com>
    Signed-off-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
    Co-authored-by: Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>

---
+++ views/HomePage/components/Partners/Partners.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from 'next-i18next';
+
+import { useMediaQuery } from 'hooks';
+
+import { Button, Container, Text } from 'components';
+
+import { Namespace } from 'i18n';
+
+import media from 'styles/media.module.scss';
+
+import styles from './Partners.module.scss';
+
+export const Partners = () => {
+  const { t } = useTranslation([Namespace.Home, Namespace.Common]);
+
+  const isLaptop = useMediaQuery({ width: { min: parseInt(media.breakpointLaptop) } });
+
+  return (
+    <Container>
+      <div className={styles.Partners}>
+        <Text className={styles.Partners__Title} type="h1" size="heading1" uppercase>
+          {t('partners.title')}
+        </Text>
+        {isLaptop ? (
+          <Text className={styles.Partners__AfterTitle} size="default">
+            {t('partners.titleAfter.upper')} <br />
+            {t('partners.titleAfter.lower')}
+          </Text>
+        ) : null}
+      </div>
+      <Text className={styles.Partners__Subtitle} type="h3" size="heading3">
+        {t('partners.subtitle')}
+      </Text>
+      <div className={styles.Partners__Slider}>
+        <Text className={styles.Partners__SliderText} type="h3" size="heading2">
+          Slider
+        </Text>
+      </div>
+      <Text className={styles.Partners__Text} type="h3" size="heading3">
+        {t('partners.text')}
+      </Text>
+      <Button className={styles.Partners__Button}>{t('becomeAClient', { ns: Namespace.Common })}</Button>
+    </Container>
+  );
+};
