0b5eede5a7a1630f3a8453b9055799f050af3e6a
IVS737 <salata.vanya191@gmail.com>
2023-02-24 17:49:20 +0200

    refactor: our services section

--- views/HomePage/components/Services/Services.tsx
+++ views/HomePage/components/Services/Services.tsx
@@ -1,9 +1,10 @@
+import { useTranslation } from 'next-i18next';
+import BoxIcon from 'public/icons/box.svg';
 import DesktopIcon from 'public/icons/desktop.svg';
 import GridIcon from 'public/icons/grid.svg';
-import ShieldCheckIcon from 'public/icons/shield-check.svg';
-import BoxIcon from 'public/icons/box.svg';
 import HashtagIcon from 'public/icons/hashtag.svg';
-import { useTranslation } from 'next-i18next';
+import ShieldCheckIcon from 'public/icons/shield-check.svg';
+import { useMemo } from 'react';
 
 import { Container, Text } from 'components';
 
@@ -13,30 +14,33 @@ import { ServiceItem } from './ServiceItem/ServiceItem';
 
 import styles from './Services.module.scss';
 
-const services = [
-  { icon: DesktopIcon, title: 'Websites' },
-  { icon: GridIcon, title: 'Mobile & web apps' },
-  { icon: ShieldCheckIcon, title: 'Software audit' },
-  { icon: BoxIcon, title: 'Back-end' },
-  { icon: HashtagIcon, title: 'Front-end' },
-];
-
 export const Services: React.FC = () => {
   const { t } = useTranslation(Namespace.Home);
 
-  return (
-    <Container>
-      <section className={styles.Services}>
-        <Text uppercase size="heading1">
-          {t('services.title')}
-        </Text>
+  const services = useMemo(
+    () => [
+      { icon: DesktopIcon, title: t('services.webDev') },
+      { icon: GridIcon, title: t('services.mobDev') },
+      { icon: ShieldCheckIcon, title: t('services.techAudit') },
+      { icon: BoxIcon, title: t('services.DevOps') },
+      { icon: HashtagIcon, title: t('services.UI/UX') },
+      { icon: HashtagIcon, title: t('services.QA') },
+      { icon: HashtagIcon, title: t('services.ItConsult') },
+    ],
+    [t],
+  );
 
-        <div className={styles.Services__Icons}>
-          {services.map(({ icon, title }) => (
-            <ServiceItem key={title} Icon={icon} title={title} />
-          ))}
-        </div>
-      </section>
+  return (
+    <Container className={styles.Services}>
+      <Text uppercase size="heading1">
+        {t('services.title')}
+      </Text>
+
+      <div className={styles.Services__Icons}>
+        {services.map(({ icon, title }) => (
+          <ServiceItem key={title} icon={icon} title={title} />
+        ))}
+      </div>
     </Container>
   );
 };
