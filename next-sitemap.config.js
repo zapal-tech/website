632b694ee0696d987e020fd209420c0c83d1a8be
--global <bohdan.kucheriavyi@zapal.tech>
2023-03-29 14:52:20 +0300

    fix: sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
+  exclude: ['*404', '*500'],
   alternateRefs: [
     {
       href: process.env.NEXT_PUBLIC_SITE_URL + '/uk',
