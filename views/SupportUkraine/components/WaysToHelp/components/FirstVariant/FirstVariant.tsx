b0085eebac17cbcfdbf7cb5206945b9b3f174aab
IVS737 <ivan.salata@zapal.tech>
2023-05-14 14:59:13 +0300

    feat: add new section

---
+++ views/SupportUkraine/components/WaysToHelp/components/FirstVariant/FirstVariant.tsx
@@ -0,0 +1,26 @@
+import { Text } from 'components';
+
+import { CharityOrganisations } from './components/CharityOrganisations/CharityOrganisations';
+
+import styles from './FirstVariant.module.scss';
+
+export const FirstVariant = () => {
+  return (
+    <>
+      <div className={styles.FirstVariant}>
+        <Text className={styles.FirstVariant__Titles} size="heading2">
+          1/ DONATE MONEY FOR HUMANITARIAN OR MILITARY RELIEF
+        </Text>
+        <Text className={styles.FirstVariant__Text} size="small">
+          Ukrainian state and charity accounts are now open for foreigners to make donations. All of the organizations
+          we are going to talk about are credible. In our article, we have included reliable links. If you search for
+          these charities and organizations yourself, please, double-check all websites, links, and account information.
+          russia’s war against Ukraine is hybrid, so there are many phishing sites and resources designed to mislead
+          donors.
+        </Text>
+      </div>
+
+      <CharityOrganisations />
+    </>
+  );
+};
