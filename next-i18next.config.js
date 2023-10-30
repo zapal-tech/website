12e48b76436522a7aa9face5e433918b860cbc42
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2023-10-30 19:46:50 +0200

    feat: update base config for the new api

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -11,12 +11,30 @@ module.exports = {
   /** @type {import('i18next-http-backend').HttpBackendOptions} **/
   backend: {
     // load: 'languageOnly',
-    loadPath: `${process.env.API_URL}/{{ns}}?locale={{lng}}&populate=deep`,
+    loadPath: `${process.env.API_URL}/globals/{{ns}}?locale={{lng}}&depth=10`,
     crossDomain: true,
-    customHeaders: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
+    customHeaders: { Authorization: process.env.API_KEY },
+    parse: (data) => {
+      const parsedData = JSON.parse(data);
+
+      if (parsedData.globalType === 'general') {
+        const general = { ...parsedData };
+
+        delete general._status;
+        delete general.globalType;
+        delete general.id;
+        delete general.createdAt;
+        delete general.updatedAt;
+
+        return general;
+      }
+
+      if (parsedData.translation) return parsedData.translation;
+
+      for (const key in parsedData) {
+        if (parsedData[key]?.translation) return parsedData[key].translation;
+      }
     },
-    parse: (data) => JSON.parse(data).data.attributes.translation,
   },
   use: [I18NextHttpBackend],
 };
