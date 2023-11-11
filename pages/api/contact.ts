7684c47da6c201587f20a1702f0561cc56b9f9a3
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2023-11-11 10:57:24 +0200

    fix: api call localhost

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,15 +1,17 @@
+import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { ContactFormDto } from 'types/contactForm';
-
-import { addContact } from 'services/api';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const data = req.body as ContactFormDto;
 
     try {
-      await addContact(data);
+      await axios.post('/contact-form-leads', data, {
+        baseURL: process.env.API_URL,
+        headers: { Authorization: process.env.API_KEY },
+      });
 
       return res.status(201).end('Created');
     } catch (error) {
